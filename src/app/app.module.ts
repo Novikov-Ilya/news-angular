3f535d0a8822db84076da9fd5486220880fc83c9
mrsmile <mr.smile_lp@mail.ru>
2019-02-03 22:07:39 +0300

    first commit. list of sources is implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopPanelComponent } from './top-panel/top-panel.component';
+import { ToolsComponent } from './tools/tools.component';
+import { Routes } from '@angular/router';
+import { NewsListComponent } from './news-list/news-list.component';
+import { NewsItemComponent } from './news-item/news-item.component';
+
+const appRuoutes: Routes = [
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopPanelComponent,
+    ToolsComponent,
+    NewsListComponent,
+    NewsItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tools/tools.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
+
+@Component({
+  selector: 'app-tools',
+  templateUrl: './tools.component.html',
+  styleUrls: ['./tools.component.css']
+})
+export class ToolsComponent implements OnInit {
+  public apiKey: string = '7a67c40c89ea4952830387b8f9e5090e';
+  public url: string = 'https://newsapi.org/v2/sources?apiKey=';
+
+  public allSources:any[];
+
+  async getAllSources(sourcesUrl: string) {
+    let response = await fetch(sourcesUrl);
+    let responseJson = await response.json();
+    this.allSources = responseJson.sources;
+    
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+    this.getAllSources(this.url + this.apiKey);
+  }
+
+}
